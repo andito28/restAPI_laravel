4838fa17381ce7d2d412c595e4dac84cbfda0fb1
andito28 <andito763@gmail.com>
2021-07-23 09:49:07 +0800

    create api for update

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Siswa;
+use Exception;
+use Illuminate\Database\QueryException;
+use Symfony\Component\HttpFoundation\Response;
+use Validator;
+
+class SiswaController extends Controller
+{
+    public function index(){
+
+        $siswa = Siswa::orderBy('created_at','DESC')->get();
+
+        $response = [
+            'message' => ' list Siswa time order by desc',
+            'data' => $siswa
+        ];
+
+        return response()->json($response,response::HTTP_OK);
+    }
+
+    public function store(request $request){
+
+        $validator = validator::make($request->all(),[
+            'nama' => 'required',
+            'nisn' => 'required',
+            'kelas' => 'required'
+        ]);
+
+        if($validator->fails()){
+
+            return response()->json($validator->errors(),response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+
+        try{
+
+            $siswa = Siswa::create($request->all());
+
+            $response = [
+                'message' => 'Siswa Created',
+                'data' => $siswa
+            ];
+
+            return response()->json($response,response::HTTP_CREATED);
+
+        }catch(QueryException $e){
+            return response()->json([
+                'message' => 'failed'.$e->errorInfo
+            ]);
+        }
+    }
+
+    public function update(request $request,$id){
+
+        $siswa = Siswa::findOrFail($id);
+
+        $validator = Validator::make($request->all(),[
+            'nama' => 'required',
+            'nisn' => 'required',
+            'kelas' => 'required'
+        ]);
+
+
+        if($validator->fails()){
+            return response()->json($validator->errors(),response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        try{
+            $siswa->update($request->all());
+
+            $response = [
+                'message' => 'Siswa updated',
+                'data' => $siswa
+            ];
+
+            return response()->json($response,response::HTTP_OK);
+
+        }catch(QueryException $e){
+
+            return  response()->json([
+                'message' => 'failed'.$e->errorInfo
+            ]);
+        }
+    }
+}
